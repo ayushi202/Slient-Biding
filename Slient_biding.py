dfe2b8e34ae988c44f414447b014f0e7b91c7a4d
ayushi202 <ayushisrivastava.dli@gmail.com>
2021-03-03 17:43:33 +0530

    commit

---
+++ Slient_biding.py
@@ -0,0 +1,35 @@
+import os
+os.system('cls')
+
+def highestbid(h):
+    maxi=0
+    for i in h:
+        if h[i]>maxi:
+                maxi=h[i]
+                name=i
+    print(f"Highest bid is of {maxi} by {name}")
+  
+print('''
+       ,
+      /(  ___________
+     |  >:===========`
+      )(
+''')        
+h={}
+bidding=True
+while bidding:
+    name=input("Enter your name: ")
+    bet=int(input("Enter your bet: $"))
+    h[name]=bet
+    print(h)
+    repeat=input("Do you want add one more bet? (type 'yes' or 'no') ")
+    if repeat=="yes":
+        os.system('cls')
+
+    else:
+        bidding=False
+        highestbid(h)
+        
+            
+    
+        
